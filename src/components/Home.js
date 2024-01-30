5b95e03283e65b214935fcd66ee1fe5036875f94
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2024-01-30 11:35:02 +0300

    aster

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,14 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const myStyle={
+    color:"firebrick"
+  }
+  return <div id="home">Home
+    <h1 style={myStyle}>
+      Your {name} is a web developer from your {city}
+    </h1>
+  </div>;
 }
 
 export default Home;
